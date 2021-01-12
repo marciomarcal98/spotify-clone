b6615e791b8ac8c2d131b334d5b45b85fbde8b09
Marcio Marçal da Costa Garcia <48536961+marciomarcal98@users.noreply.github.com>
2021-01-12 11:15:01 -0300

    Add files via upload

---
+++ src/SongRow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./SongRow.css";
+
+function SongRow({ track, playSong }) {
+  console.log(track);
+  return (
+    <div className="songRow" onClick={() => playSong(track.id)}>
+      <img className="songRow__album" src={track.album.images[0].url} alt=""/>
+      <div className="songRow__info">
+        <h1>{track.name}</h1>
+        <p>
+          {track.artists.map((artist) => artist.name).join(", ")}<br/>
+          <i>{track.album.name}</i>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SongRow;
---
+++ src/reducer.js
@@ -0,0 +1,70 @@
+import { findAllByDisplayValue } from "@testing-library/react";
+
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  // Remove after finished developing...
+  // token: 'BQD8UH5Bv6mypJDhuC0RGa2JX8IKbQxBDr5BjlN2d6fuAOJsaQ…fPNLrXJT6D1rRgPdcyndIFTR34awJVZlvDhqcK29UxgqVAdnJ',
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch(action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user
+      };
+    
+    case 'SET_PLAYING':
+      return{
+        ...state,
+        playing: action.playing,
+      };
+    
+    case 'SET_ITEM':
+      return {
+        ...state,
+        item: action.item,
+      };  
+
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token
+      };
+
+    case 'SET_PLAYLISTS':
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    
+    case 'SET_DISCOVER_WEEKLY':
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case 'SET_TOP_ARTISTS':
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case 'SET_SPOTIFY':
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+    default: 
+      return state;
+  }
+};
+
+export default reducer;
